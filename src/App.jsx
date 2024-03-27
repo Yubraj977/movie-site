116fd8d4a144bbebef2e0eb0ca2d99cd72410297
Yubraj977 <yubrajkhatri977@gmail.com>
2024-03-27 17:27:38 +0545

    setup complete

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { createBrowserRouter ,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
+import Nav from './elements/pages/Nav'
+import Home from './elements/pages/Home'
+import About from './elements/pages/About'
+import Comedy from './elements/pages/Comedy'
+import Tvshows from './elements/pages/Tvshows'
+
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Nav/>}>
+
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='comedy' element={<Comedy/>}/>
+      <Route path='tvshows' element={<Tvshows/>}/>
+    </Route>
+  )
+)
+
+function App() {
+
+
+  return (
+    <div>
+  
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default App
