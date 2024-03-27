6d5c2f609ff56969121e619ec8bbbea25ad22e09
Yubraj977 <yubrajkhatri977@gmail.com>
2024-03-27 19:05:57 +0545

    a lot of best options

--- src/elements/pages/Home.jsx
+++ src/elements/pages/Home.jsx
@@ -1,31 +1,56 @@
 import React from 'react'
+import { FaArrowRight } from "react-icons/fa";
+import { CgSearch } from "react-icons/cg";
+import Card from './components/Card';
 
 function Home() {
-  return (
-    <div className='flex  flex-col items-center'>
+    return (
+        <div className='flex  flex-col items-center mt-12' >
 
             <div className='top'>
-                <h1 className='text-white font-3xl'>Find Moives Tv shows and more</h1>
+                <h1 className='text-white text-3xl'>Find Moives Tv shows Download and enjoy</h1>
             </div>
 
 
-<div className="search relative">
-    
-    <input type="text" name="" id=""  className='rounded-lg'/>
-</div>
-      
+
+            <div className="search relative w-[50rem] flex justify-center gap-2 mt-4">
+               
+                    <CgSearch className='absolute top-1/2 -translate-y-1/2 font-extrabold  left-1' />
+                    <input type="text" name="" id="" className='rounded-3xl w-full h-16 pl-8' placeholder='Enter the movie name ' />
+                   
+                   <div className='bg-white rounded-full flex justify-center items-center w-12'> <FaArrowRight /> </div>
+               
+            </div>
 
 
 
 
-            <div></div>
-            <div></div>
+
+
+            <div>
+                <p className='text-white font-bold px-40 mt-4'>
+                    This is the website where you can download any kind of movies as per your perferieenc happy entertainment
+                    This is the website where you can download any kind of movies as per your perferieenc happy entertainment
+                    </p>
+            </div>
+            <div> </div>
 
 
 
-            <div> movie Render....</div>
-    </div>
-  )
+            <div className='flex flex-wrap gap-3 justify-center mt-10'> 
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+        </div>
+    )
 }
 
 export default Home
